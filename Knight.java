7cf5d490c84a987e31d2bfa43c1be5f27edaec3e
kvsc21 <karanvirchib@gmail.com>
2020-08-20 18:10:54 +0530

    first comit

---
+++ Knight.java
@@ -0,0 +1,15 @@
+class Knight implements SwordFighter {
+
+    @Override
+    public void showWeapon() {
+        System.out.println("Shows Sword");
+    }
+    public void escape()
+        {
+        System.out.println("Knight climbs the horse and flees");
+    }
+    public void attack()
+    {
+        System.out.println("Knight stops the horse and attacks");
+    }
+}
